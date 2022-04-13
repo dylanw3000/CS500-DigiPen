57c9b2504daceafb911e779171662ba50800a866
dylanw3000 <dylanw3000@gmail.com>
2022-04-13 15:52:25 -0700

    moved around texture

--- CS500-framework/Texture.cpp
+++ CS500-framework/Texture.cpp
@@ -1,4 +1,26 @@
 
+#include "Texture.h"
+
+_Image::_Image(const char* filename) {
+    // stbi_set_flip_vertically_on_load(true);
+    img = stbi_load(filename, &width, &height, &channels, 3);
+    if (img == NULL) {
+        std::cout << "Image failed to load" << std::endl;
+        exit(1);
+    }
+}
+
+vec3 _Image::getUV(float u, float v) {
+    // Intentionally incomplete, not rounding up or linearly interpolating color
+    u = clamp(u, 0.f, 1.f);
+    int x = (width - 1) * u;
+    v = clamp(v, 0.f, 1.f);
+    int y = (height - 1) * v;
+
+    int index = channels * (x + y * width);
+    return vec3(img[index], img[index + 1], img[index + 2]) / 255.f;
+}
+
 #if false
 
 #include <glbinding/gl/gl.h>
--- CS500-framework/Texture.h
+++ CS500-framework/Texture.h
@@ -1,31 +1,19 @@
 #pragma once
 
 #include <stb_image.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
+using namespace glm;
 
 class _Image {
 public:
     int height, width, channels;
     uint8_t* img;
 
-    _Image(const char* filename) {
-        stbi_set_flip_vertically_on_load(true);
-        img = stbi_load(filename, &width, &height, &channels, 3);
-        if (img == NULL) {
-            std::cout << "Image failed to load" << std::endl;
-            exit(1);
-        }
-    }
+    _Image(const char* filename);
 
-    vec3 getUV(float u, float v) {
-        // Intentionally incomplete, not rounding up or linearly interpolating color
-        u = clamp(u, 0.f, 1.f);
-        int x = (width-1) * u;
-        v = clamp(v, 0.f, 1.f);
-        int y = (height-1) * v;
-
-        int index = channels * (x + y * width);
-        return vec3(img[index], img[index + 1], img[index + 2]) / 256.f;
-    }
+    vec3 getUV(float u, float v);
 
     vec3 getUV(vec2 uv) { return getUV(uv.x, uv.y); }
 };
